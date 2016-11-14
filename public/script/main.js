ebd904c49685bdb7585e9a0e4c9ecf04c5def3c6
Ryan P Brewster <ryan@kifi.com>
2016-11-13 16:53:38 -0800

    Get user authentication to work

--- public/script/main.js
+++ public/script/main.js
@@ -1,36 +1,65 @@
 var rootRef = firebase.database().ref();
 var messageRef = rootRef.child("messages");
+var userRef = rootRef.child("users");
 
 var loginButton = document.getElementById("login");
 var logoutButton = document.getElementById("logout");
 var commentContainer = document.getElementById("comment-container");
 var commentInput = document.getElementById("comment-input");
 
+var googleAuth = new firebase.auth.GoogleAuthProvider();
+var user = null;
+
+function canonicalizeUser() {
+  userRef.child(user.uid).set({
+    displayName: user.displayName,
+    photoUrl: user.photoURL
+  });
+};
+
 loginButton.onclick = function() {
-  console.log("login clicked");
+  firebase.auth().signInWithPopup(googleAuth);
 };
 
 logoutButton.onclick = function() {
-  console.log("logout clicked");
+  firebase.auth().signOut();
 };
 
 commentInput.onkeypress = function(evt) {
-  if (evt.code === "Enter" && commentInput.value.length > 0) {
+  if (user && evt.code === "Enter" && commentInput.value.length > 0) {
     pushComment(commentInput.value);
     commentInput.value = "";
   }
 };
 
 function pushComment(txt) {
-  messageRef.push({"text": txt});
+  messageRef.push({"text": txt, "author": user.uid});
 }
 
-function addCommentElement(key, text) {
+function addCommentElement(key, comment) {
   var commentElement = document.createElement("div");
-  commentElement.innerText = text;
-  commentContainer.appendChild(commentElement);
+  if (!comment.author) {
+    commentElement.innerText = "someone: " + comment.text;
+    commentContainer.appendChild(commentElement);
+  } else {
+    userRef.child(comment.author).once("value").then(function (authorSnapshot) {
+      var author = authorSnapshot.val();
+      commentElement.innerText = author.displayName + ": " + comment.text;
+      commentContainer.appendChild(commentElement);
+    });
+  }
 }
 
 messageRef.on("child_added", function(data) {
-  addCommentElement(data.key, data.val().text);
+  addCommentElement(data.key, data.val());
+});
+
+firebase.auth().onAuthStateChanged(function (signedInUser) {
+  user = signedInUser;
+  if (user) {
+    canonicalizeUser();
+    commentInput.disabled = false;
+  } else {
+    commentInput.disabled = true;
+  }
 });
